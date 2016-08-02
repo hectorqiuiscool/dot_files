037f1c6e360b7a2a3bee6f41b91b4aec759764ca
hectorqiu <hectorqiuiscool@gmail.com>
2016-08-02 15:55:33 +0800

    update vim configs

--- .ShadowsocksX/gfwlist.js
+++ .ShadowsocksX/gfwlist.js
@@ -4,6 +4,8 @@
 var proxy = "SOCKS5 127.0.0.1:1080; SOCKS 127.0.0.1:1080; DIRECT;";
 
 var rules = [
+  "||stackoverflow.com",
+  "||imgur.com",
   "||w3schools.com",
   "||elastic.co",
   "||digitalocean.com",
